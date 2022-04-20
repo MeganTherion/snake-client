137b63d403934179b9157e787972c04aa3c4663e
MeganTherion <meganhutt@hotmail.com>
2022-04-20 21:28:13 +0000

    adding event listener to play.js

--- client.js
+++ client.js
@@ -19,11 +19,8 @@ const connect = function() {
   conn.on('data', (data) => {
     console.log(data);
   });
-
-
-  //interpret incoming data as text
+//interpret incoming data as text
   conn.setEncoding("utf8");
-  
   
   return conn;
 };
--- play.js
+++ play.js
@@ -1,11 +1,30 @@
 
 const connect = require("./client");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function(data) {
+    if (data === '\u0003')
+    process.exit();
+    
+  }
+
+  stdin.on("data", handleUserInput);
+
+ 
+  return stdin;
+};
 
 
 console.log("connecting...");
 
 connect();
+setupInput();
+
 
 
 
